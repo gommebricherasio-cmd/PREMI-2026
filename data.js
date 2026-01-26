24131f4bf7131fda14ce981f6cf11dfc45dcfdfe
gommebricherasio-cmd <gommebricherasio@gmail.com>
2026-01-26 12:02:40 +0100

    Aggiornamento automatico dashboard data.js

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 window.dashboardData = {
     "current_month": "GENNAIO",
-    "avanzamento": 0.0816,
-    "premialita": 635,
-    "avanzamento_annuale": 4.49,
+    "avanzamento": -27.55,
+    "premialita": 0,
+    "avanzamento_annuale": 3.01,
     "reclami": 4,
     "year_2025": 22665.0,
-    "year_2026": 25741.03,
+    "year_2026": 17241.03,
     "history": {
         "months": [
             "GENNAIO",
@@ -23,7 +23,7 @@ window.dashboardData = {
             "TOTALE 2026"
         ],
         "avanzamento": [
-            0.1357171850871387,
+            -0.23931039046988753,
             -1.0,
             -1.0,
             -1.0,
@@ -35,10 +35,10 @@ window.dashboardData = {
             -1.0,
             -1.0,
             -1.0,
-            -0.952872778069692
+            -0.9684347577732089
         ],
         "premialita": [
-            635.72,
+            0.0,
             NaN,
             NaN,
             NaN,
