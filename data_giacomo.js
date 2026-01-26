66e2bbef7928a3395388707a3da9fd019eeed006
gommebricherasio-cmd <gommebricherasio@gmail.com>
2026-01-26 08:40:40 +0100

    Aggiornamento automatico dashboard data_giacomo.js

---
+++ data_giacomo.js
@@ -0,0 +1,14 @@
+window.mechanicData = {
+    "latest": {
+        "month": "GEN",
+        "giacomo": {
+            "commessa": 0,
+            "vendute": 41.44,
+            "premio": 0.0
+        }
+    },
+    "months": [],
+    "giacomo": {
+        "premio": []
+    }
+};
