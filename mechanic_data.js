8600d137bb6c48c60cedfaa43ed9f316ad965b40
gommebricherasio-cmd <gommebricherasio@gmail.com>
2026-03-05 06:59:03 +0100

    Aggiornamento automatico dashboard mechanic_data.js

--- mechanic_data.js
+++ mechanic_data.js
@@ -2,25 +2,25 @@ window.mechanicData = {
     "latest": {
         "month": "FEB",
         "totals": {
-            "vendute": 144.01999999999998,
-            "utile": 2537.69,
+            "vendute": 195.26999999999998,
+            "utile": 3100.99,
             "premio": 0.0
         },
         "simone": {
             "commessa": 0,
-            "vendute": 59.79,
+            "vendute": 85.53999999999999,
             "premio": 0.0,
-            "utile": 1501.52
+            "utile": 1746.6699999999998
         },
         "giacomo": {
             "commessa": 0,
-            "vendute": 84.22999999999999,
+            "vendute": 109.72999999999999,
             "premio": 0.0,
-            "utile": 1036.1699999999998
+            "utile": 1354.32
         },
         "gabriele": {
             "commessa": 0,
-            "vendute": 144.01999999999998,
+            "vendute": 195.26999999999998,
             "premio": 0.0
         }
     },
@@ -35,7 +35,7 @@ window.mechanicData = {
         ],
         "vendute": [
             133.93666666666667,
-            59.79
+            85.53999999999999
         ]
     },
     "giacomo": {
@@ -45,7 +45,7 @@ window.mechanicData = {
         ],
         "vendute": [
             92.21333333333332,
-            84.22999999999999
+            109.72999999999999
         ]
     },
     "gabriele": {
@@ -55,13 +55,13 @@ window.mechanicData = {
         ],
         "vendute": [
             226.14999999999998,
-            144.01999999999998
+            195.26999999999998
         ]
     },
     "totals": {
         "utile": [
             2872.56,
-            2537.69
+            3100.99
         ]
     },
     "annual_premium": 639.91
