40c526c350474d99eb7eccfbf9a660233a994f33
gommebricherasio-cmd <gommebricherasio@gmail.com>
2026-03-06 13:12:01 +0100

    Aggiornamento automatico dashboard mechanic_data.js

--- mechanic_data.js
+++ mechanic_data.js
@@ -1,67 +1,75 @@
 window.mechanicData = {
     "latest": {
-        "month": "FEB",
+        "month": "MAR",
         "totals": {
-            "vendute": 197.57,
-            "utile": 3166.3199999999997,
+            "vendute": 29.6,
+            "utile": 536.3,
             "premio": 0.0
         },
         "simone": {
             "commessa": 0,
-            "vendute": 87.83999999999999,
+            "vendute": 15.51,
             "premio": 0.0,
-            "utile": 1811.9999999999998
+            "utile": 266.81
         },
         "giacomo": {
             "commessa": 0,
-            "vendute": 109.72999999999999,
+            "vendute": 14.09,
             "premio": 0.0,
-            "utile": 1354.32
+            "utile": 269.49
         },
         "gabriele": {
             "commessa": 0,
-            "vendute": 197.57,
+            "vendute": 29.6,
             "premio": 0.0
         }
     },
     "months": [
         "GEN",
-        "FEB"
+        "FEB",
+        "MAR"
     ],
     "simone": {
         "premio": [
+            0.0,
             0.0,
             0.0
         ],
         "vendute": [
             133.93666666666667,
-            87.83999999999999
+            87.83999999999999,
+            15.51
         ]
     },
     "giacomo": {
         "premio": [
             0.0,
+            0.0,
             0.0
         ],
         "vendute": [
             92.21333333333332,
-            109.72999999999999
+            109.72999999999999,
+            14.09
         ]
     },
     "gabriele": {
         "premio": [
             0.0,
+            0.0,
             0.0
         ],
         "vendute": [
             226.14999999999998,
-            197.57
+            197.57,
+            29.6
         ]
     },
     "totals": {
         "utile": [
             2872.56,
-            3166.3199999999997
+            3166.3199999999997,
+            536.3
         ]
     },
     "annual_premium": 639.91
