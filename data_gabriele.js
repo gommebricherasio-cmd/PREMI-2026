b556c74e90fe3967ecfd81d3b183d666440550ed
gommebricherasio-cmd <gommebricherasio@gmail.com>
2026-03-06 13:12:17 +0100

    Aggiornamento automatico dashboard data_gabriele.js

--- data_gabriele.js
+++ data_gabriele.js
@@ -1,24 +1,27 @@
 window.mechanicData = {
     "latest": {
-        "month": "FEB",
+        "month": "MAR",
         "gabriele": {
             "commessa": 0,
-            "vendute": 197.57,
+            "vendute": 29.6,
             "premio": 0.0
         }
     },
     "months": [
         "GEN",
-        "FEB"
+        "FEB",
+        "MAR"
     ],
     "gabriele": {
         "premio": [
+            0.0,
             0.0,
             0.0
         ],
         "vendute": [
             226.14999999999998,
-            197.57
+            197.57,
+            29.6
         ]
     },
     "annual_premium": 639.91
