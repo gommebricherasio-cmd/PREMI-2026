a7ce8828db4c1cff2c1f8b551a522f71a2dcf468
gommebricherasio-cmd <gommebricherasio@gmail.com>
2026-04-01 07:14:22 +0200

    Aggiornamento automatico dashboard mechanic_data.js

--- mechanic_data.js
+++ mechanic_data.js
@@ -1,36 +1,38 @@
 window.mechanicData = {
     "latest": {
-        "month": "MAR",
+        "month": "APR",
         "totals": {
-            "vendute": 169.95000000000002,
-            "utile": 3041.71,
+            "vendute": 0.0,
+            "utile": 0.0,
             "premio": 0.0
         },
         "simone": {
             "commessa": 0,
-            "vendute": 71.42,
+            "vendute": 0.0,
             "premio": 0.0,
-            "utile": 1299.0900000000001
+            "utile": 0.0
         },
         "giacomo": {
             "commessa": 0,
-            "vendute": 98.53000000000002,
+            "vendute": 0.0,
             "premio": 0.0,
-            "utile": 1742.62
+            "utile": 0.0
         },
         "gabriele": {
             "commessa": 0,
-            "vendute": 169.95000000000002,
+            "vendute": 0.0,
             "premio": 0.0
         }
     },
     "months": [
         "GEN",
         "FEB",
-        "MAR"
+        "MAR",
+        "APR"
     ],
     "simone": {
         "premio": [
+            0.0,
             0.0,
             0.0,
             0.0
@@ -38,11 +40,13 @@ window.mechanicData = {
         "vendute": [
             133.93666666666667,
             89.61333333333333,
-            71.42
+            71.42,
+            0.0
         ]
     },
     "giacomo": {
         "premio": [
+            0.0,
             0.0,
             0.0,
             0.0
@@ -50,11 +54,13 @@ window.mechanicData = {
         "vendute": [
             92.21333333333332,
             111.95666666666668,
-            98.53000000000002
+            98.53000000000002,
+            0.0
         ]
     },
     "gabriele": {
         "premio": [
+            0.0,
             0.0,
             0.0,
             0.0
@@ -62,14 +68,16 @@ window.mechanicData = {
         "vendute": [
             226.14999999999998,
             201.57,
-            169.95000000000002
+            169.95000000000002,
+            0.0
         ]
     },
     "totals": {
         "utile": [
             2872.56,
             3279.7700000000004,
-            3041.71
+            3041.71,
+            0.0
         ]
     },
     "annual_premium": 639.91
