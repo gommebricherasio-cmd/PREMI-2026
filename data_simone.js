cd20de350c8a269f62d3e288f1c5d6ccff8c2f23
gommebricherasio-cmd <gommebricherasio@gmail.com>
2026-04-30 11:16:57 +0200

    Aggiornamento automatico dashboard data_simone.js

--- data_simone.js
+++ data_simone.js
@@ -3,9 +3,9 @@ window.mechanicData = {
         "month": "APR",
         "simone": {
             "commessa": 0,
-            "vendute": 92.74333333333333,
+            "vendute": 96.28999999999999,
             "premio": 0.0,
-            "utile": 1503.5000000000002
+            "utile": 1728.9200000000003
         }
     },
     "months": [
@@ -25,7 +25,7 @@ window.mechanicData = {
             133.93666666666667,
             89.61333333333333,
             96.33,
-            92.74333333333333
+            96.28999999999999
         ]
     },
     "annual_premium": 639.91
